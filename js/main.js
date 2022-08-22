8c2ec17d337b8f33b5c98c3d0a1349e81f12f9f2
Drijon <drinihalili15@gmail.com>
2022-08-22 12:58:26 +0200

    code format because this is an old project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mins =0;
+var mins = 0;
 var sec = 0;
 var ms = 0;
 var Interval;
@@ -11,77 +11,63 @@ var time = document.getElementsByClassName("time-container");
 var cont = document.getElementById("main-container");
 
 var play = document.getElementById("play");
-   
-  
+
+
 function animation() {
 	cont.style.animation = 'coloranimation 1s infinite';
-
 }
 
 
-function stopanimation(){ 
-
-cont.style.animation = 'coloranimation 0s 0';
-
+function stopanimation() {
+	cont.style.animation = 'coloranimation 0s 0';
 }
 
 
-function start(){
+function start() {
+	ms++;
+	if (ms <= 9) {
+		mseconds.innerHTML = "0" + ms;
+	}
 
-ms++;
+	if (ms > 9) {
+		mseconds.innerHTML = ms;
+	}
 
-if (ms<=9) {
-	mseconds.innerHTML = "0"+ms;
-}
+	if (ms > 99) {
+		sec++;
+		seconds.innerHTML = sec + " :";
+		ms = 0;
+		mseconds.innerHTML = "0" + ms;
+	}
 
-if(ms>9){
-	mseconds.innerHTML=ms;
-}
+	if (sec <= 9) {
+		seconds.innerHTML = "0" + sec + " :";
+	}
 
-if(ms>99){
-	sec++;
-	seconds.innerHTML =sec+" :";
-	ms=0;
-	mseconds.innerHTML="0"+ms;
-}
+	if (sec > 9) {
+		seconds.innerHTML = sec + " :";
+	}
 
+	if (sec > 59) {
+		mins++;
+		sec = 0;
+		minutes.innerHTML = mins + " :";
+	}
 
-if(sec<=9){
-seconds.innerHTML = "0" + sec+" :";
-}
+	if (mins <= 9) {
+		minutes.innerHTML = "0" + mins + " :";
+	}
 
-if(sec>9){
-seconds.innerHTML = sec+" :";
-}
+	if (mins > 9) {
+		minutes.innerHTML = mins + " :";
+	}
 
-
-
-if(sec>59){
-	mins++;
-	sec=0;
-	minutes.innerHTML=mins+" :";
-     
-}
-
-
-if(mins<=9){
-	minutes.innerHTML = "0" + mins+" :";
-}
-
-if(mins>9){
-	minutes.innerHTML = mins+" :";
-}
-
-if(mins>59){
-	mins = 0;
-
-}
-
-animation();
-clearInterval(Interval);
-Interval = setInterval(start, 9);
-
-
+	if (mins > 59) {
+		mins = 0;
+	}
+	animation();
+	clearInterval(Interval);
+	Interval = setInterval(start, 9);
 }
 
 
@@ -90,14 +76,13 @@ function stop() {
 	stopanimation();
 }
 
-
 function reset() {
-	mins=0;
-	sec=0;
-	ms=0;
-	minutes.innerHTML ='0'+ mins+" :";
-	seconds.innerHTML = '0'+sec+" :";
-	mseconds.innerHTML='0'+ms;
+	mins = 0;
+	sec = 0;
+	ms = 0;
+	minutes.innerHTML = '0' + mins + " :";
+	seconds.innerHTML = '0' + sec + " :";
+	mseconds.innerHTML = '0' + ms;
 	clearInterval(Interval);
 	stopanimation();
 }
